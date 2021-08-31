0d2f3bd7e57b224658ab98b49d7a69a2e565a716
Mauricio Maroto <maulberto3@hotmail.com>
2021-08-30 23:25:49 -0600

    ex02

--- ex02.py
+++ ex02.py
@@ -1,10 +1,14 @@
-def calc_manual_sum(*nums):
+def calc_manual_sum(*nums, other=0):
     total = 0
     for num in nums:
         total += num
+    if other:
+        total += other
     print(total)
     return total
 
 
 calc_manual_sum(1, 2, 3)
 calc_manual_sum(*[1, 2, 3])
+calc_manual_sum(1, 2, 3, other=1)
+calc_manual_sum(*[1, 2, 3], other=1)
